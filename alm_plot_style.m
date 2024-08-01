a1ef3c6ae18d993531d1ad9061b77c8a8962ba40
Rana X <rana@caltech.edu>
2024-08-01 15:42:03 -0700

    run this to have a nice plot style

---
+++ alm_plot_style.m
@@ -0,0 +1,15 @@
+% custom_plot_style.m
+function custom_plot_style()
+    % Set default properties for MATLAB plots
+    set(0, 'DefaultLineLineWidth', 2);  % Thicker lines
+    set(0, 'DefaultAxesFontSize', 14);  % Larger font size for axes labels
+    set(0, 'DefaultAxesFontWeight', 'bold');  % Bold font for axes labels
+    set(0, 'DefaultAxesTitleFontSizeMultiplier', 1.2);  % Larger title font
+    set(0, 'DefaultAxesLabelFontSizeMultiplier', 1.2);  % Larger labels font
+    set(0, 'DefaultTextFontSize', 14);  % Larger font for text
+    set(0, 'DefaultTextFontWeight', 'bold');  % Bold font for text
+    set(0, 'DefaultFigureColor', 'w');  % White background for figures
+    set(0, 'DefaultAxesLineWidth', 1.5);  % Thicker axes lines
+    set(0, 'DefaultAxesBox', 'on');  % Box around axes
+    set(0, 'DefaultLegendFontSize', 14);  % Larger font size for legends
+end
